c38793a568ac935d86662dd88701606ba60e2ab3
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-04-30 19:24:36 +0000

    Add sign up route to sign in page
    Add styling to sign in/ sign up pages

---
+++ src/components/SignIn.jsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+
+function SignIn() {
+  return (
+    <>
+      <section className="sign-section">
+        <h1>Sign In</h1>
+        <form className="sign-form">
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <Link className="button">Sign In</Link>
+          <small>
+            Don&apos;t have an account?
+            <strong>
+              <Link to="/sign-up">Sign Up</Link>
+            </strong>
+          </small>
+        </form>
+      </section>
+    </>
+  );
+}
+
+export default SignIn;
