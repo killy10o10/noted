7606f0b86e9fdde201b3f66506b0ae59c0171148
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2023-05-02 08:45:23 +0000

    Add todo componment

---
+++ src/components/Todos.jsx
@@ -0,0 +1,16 @@
+function Todos() {
+  return (
+    <>
+      <section className="todo-section">
+          <h3>What&apos;s up Quami</h3>
+          <div className="todo-input">
+            <form className="todo-form">
+              <input type="text" placeholder="eg. Walk the dog" name="todo" />
+            </form>
+          </div>
+      </section>
+    </>
+  )
+}
+
+export default Todos
